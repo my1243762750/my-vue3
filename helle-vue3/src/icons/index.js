d0f02981514b768405f8b22bbcb081cb1ff336eb
jamei <jamei@deloitte.com.cn>
2021-04-20 16:25:36 +0800

    1初始化vue3项目

--- helle-vue3/src/icons/index.js
+++ helle-vue3/src/icons/index.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 import SvgIcon from '@/components/SvgIcon'// svg component
 
 // register globally
-Vue.component('svg-icon', SvgIcon)
+// Vue.component('svg-icon', SvgIcon)
 
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
+
+export default (app) => {
+  app.component('svg-icon', SvgIcon)
+}
--- helle-vue3/src/main.js
+++ helle-vue3/src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import installElementPlus from './plugins/element'
 import router from './router/index'
 import store from './store'
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import '@/styles/index.scss' // global css
+import installSvgIcon from './icons' // icon
+// import '@/permission' // permission control
 
 const app = createApp(App).use(store).use(router)
 installElementPlus(app)
+installSvgIcon(app)
 app.mount('#app')
